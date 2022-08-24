6406407ee02ab6976cc504914cb7765497b08153
nattyKhoe <natasha.kusuma@outlook.com>
2022-08-24 18:42:46 +1000

    not passing 4 and 5

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,13 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinateButton extends React.Component {
+
+    render() {
+        return (
+        <button onClick={(e)=>this.props.onReceiveCoordinates([e.screenX, e.screenY])}></button>
+        )
+    }
+}
+
+export default CoordinateButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,11 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component {
+    render() {
+        return(
+        <button onClick={(e) => setTimeout(this.props.onDelayedClick(e), this.props.delay)}></button>
+        )
+    }
+}
+export default DelayedButton;
